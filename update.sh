f8089a42bdba0e46a5399177c3c2f13b28786db3
HaidaraIB <nezarmaarof@gmail.com>
2026-08-21 00:27:37 +0300

    feat: Add service status checks and restart for crm-qcluster in update script

--- update.sh
+++ update.sh
@@ -4,3 +4,6 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 crontab crontab_complete.txt
 sudo systemctl restart crm-api
+sudo systemctl restart crm-qcluster
+systemctl is-active --quiet crm-api     && echo "crm-api: running"
+systemctl is-active --quiet crm-qcluster && echo "crm-qcluster: running"
